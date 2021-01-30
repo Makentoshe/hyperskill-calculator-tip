c5207aa0890e774623e06b5293dba27ab10fdf9b
Makentoshe <mkliverout@gmail.com>
2021-01-30 16:00:57 +0300

    feat: Add decimal numbers processing
    Update tests and screenshots

--- Tip calculator/stage3/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
+++ Tip calculator/stage3/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
@@ -7,25 +7,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         slider.addOnChangeListener { _, percentage, _ ->
-            val value = edit_text.text.toString().toLongOrNull()
+            val value = edit_text.text.toString().toDoubleOrNull()
                     ?: return@addOnChangeListener text_view.setText("")
             display(value, percentage.toInt())
         }
 
         edit_text.doAfterTextChanged {
-            val value = it?.toString()?.toLongOrNull()
+            val value = it?.toString()?.toDoubleOrNull()
                     ?: return@doAfterTextChanged text_view.setText("")
             display(value, slider.value.toInt())
         }
     }
 
-    private fun display(bill: Long, percentage: Int) {
+    private fun display(bill: Double, percentage: Int) {
         text_view.text = getString(R.string.display, bill * percentage / 100)
     }
 }
